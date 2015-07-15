30049452d426759da744eacb882d8b3cfc8bb3f5
AmarEkanO <menezes.amar@gmail.com>
2015-07-15 19:53:14 +0100

    Updates

--- Socket-client.c
+++ Socket-client.c
@@ -10,10 +10,15 @@
 #include <string.h>
 
 
-int main(void){
+int main(int argc, char* argv[]){
 	int sockfd = 0, n = 0;
 	char recvBuff[1024];
 	struct sockaddr_in serv_addr;
+
+	if(argc != 2){
+		printf("\n Usage: %s <ip of server> \n", argv[0]);
+		return 1;
+	}
 
 	memset(recvBuff, '0', sizeof(recvBuff));
 	if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0){
@@ -21,13 +26,18 @@ int main(void){
 		return 1;
 	}
 
+	memset(&serv_addr, '0', sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_port = htons(5000);
-	serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	
+	if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr) <= 0){
+		printf("\n inet_pton error occured\n");
+		return 1;
+	}
 
 	if(connect(sockfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr))<0){
 		printf("\n Error: Connect Failed \n");
-		return -1;
+		return 1;
 	}
 
 	while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)	{
--- Socket-server.c
+++ Socket-server.c
@@ -7,40 +7,52 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h> 
 
-int main(void){
-	int listenfd = 0, connfd = 0;
+int main(int argc, char *argv[])
+{
+    int listenfd = 0, connfd = 0;
+    struct sockaddr_in serv_addr; 
+    
 
-	struct sockaddr_in serv_addr;
+    char sendBuff[1025];
+    time_t ticks; 
 
-	char sendBuff[1025];
-	//int numrv;
+    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    memset(sendBuff, '0', sizeof(sendBuff)); 
 
-	listenfd = socket(AF_INET, SOCK_STREAM, 0);
-	printf("socket retrieve success\n");
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serv_addr.sin_port = htons(5000); 
 
-	memset(&serv_addr, '0', sizeof(serv_addr));
-	memset(sendBuff, '0', sizeof(sendBuff));
+    bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)); 
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-	serv_addr.sin_port = htons(5000);
+    if(listen(listenfd, 10) == -1){
+        printf("\nUnable to listen to port 5000\n");
+    }else{
+        printf("\nListening on port 5000\n");
+    }
 
-	bind(listenfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    while(1)
+    {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        connfd = accept(listenfd, (struct sockaddr*)&client_addr, &client_addr_len); 
 
-	if(listen(listenfd, 10) == -1){
-		printf("Failed to listen\n");
-		return -1;
-	}
+        ticks = time(NULL);
+        snprintf(sendBuff, sizeof(sendBuff), "%.24s\r\n", ctime(&ticks));
+        write(connfd, sendBuff, strlen(sendBuff)); 
 
-	while(1){
-		connfd = accept(listenfd, (struct sockaddr*)NULL, NULL);
-		strcpy(sendBuff, "Message from server");
-		write(connfd, sendBuff, strlen(sendBuff));
+        char client_name[INET_ADDRSTRLEN];
+        if(inet_ntop(AF_INET, &client_addr.sin_addr.s_addr, client_name, sizeof(client_name)) != NULL){
+            printf("Handling client %s:%d\n", client_name, ntohs(client_addr.sin_port));
+        }else{
+            puts("Unable to get client address");
+        }
 
-		close(connfd);
-		sleep(1);
-	}
 
-	return 0;
+        close(connfd);
+        sleep(1);
+     }
 }
